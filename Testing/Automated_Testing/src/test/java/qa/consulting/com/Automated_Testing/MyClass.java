b7075bdc93ad5251b0f850e6a16b59174c0bcedb
michael.brewster@qa.com <michael.brewster@qa.com>
2017-11-27 12:11:04 +0000

    Created MyClass in test files

---
+++ Testing/Automated_Testing/src/test/java/qa/consulting/com/Automated_Testing/MyClass.java
@@ -0,0 +1,49 @@
+package qa.consulting.com.Automated_Testing;
+import org.junit.*;
+
+
+public class MyClass {
+	
+	@BeforeClass
+	public static void beforeClass()
+	{
+		// Runs at the start -- before every thing
+		System.out.println( "Before Class" );
+	}
+	
+	@Before
+	public void before()
+	{
+		// runs before every test
+		System.out.println( "Before" );
+	}
+	
+	@Test
+	public void test()
+	{
+		// the test
+		System.out.println( "Test" );
+	}
+	
+	@After
+	public void after()
+	{
+		// runs after every test
+		System.out.println( "After" );
+	}
+	
+	@Test
+	public void hello()
+	{
+		// the test
+		System.out.println( "Hello World!" );
+	}
+	
+	@AfterClass
+	public static void afterClass()
+	{
+		// runs last -- after every thing 
+		System.out.println( "After Class" );
+	}
+
+}
