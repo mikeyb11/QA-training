9f7600622462eb8cf80dacbd2858426f69c0dd43
michael.brewster@qa.com <michael.brewster@qa.com>
2017-11-28 12:31:46 +0000

    Created Selenium test files

--- Testing/Automated_Testing/src/test/java/qa/consulting/com/Automated_Testing/DemoSiteLogin.java
+++ Testing/Automated_Testing/src/test/java/qa/consulting/com/Automated_Testing/DemoSiteLogin.java
@@ -2,6 +2,11 @@ package qa.consulting.com.Automated_Testing;
 
 import static org.junit.Assert.*;
 
+import java.awt.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -14,20 +19,38 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class DemoSiteLogin {
 	private String url = "http://thedemosite.co.uk";
 	private WebDriver webDriver;
 
-	private String userNametxt = "mike";
-	private String pwordtxt = "mike";
+	private String userNametxt;// = "mike";
+	private String pwordtxt;// = "mike";
 	
 	private Homepage mHomePage;// = PageFactory.initElements(webDriver, Homepage.class);
+	
+	private static ExtentReports report;
+	private ExtentTest test = report.createTest("DemoSiteReports");
+		
+	private String mLookupDoc = "lookups.xlsx";
+	private SpreadSheetReader mReader;// = new SpreadSheetReader(mLookupDoc);
 	
 	@BeforeClass
 	public static void beforeClass()
 	{
 		// Runs at the start -- before every thing
 		System.out.println( "Before Class" );
+		
+		// init the report
+		report = new ExtentReports();
+        String fileName = "MyReport" + ".html";
+        String filePath = System.getProperty("user.dir")
+                + File.separatorChar + fileName;
+        report.attachReporter(new ExtentHtmlReporter(filePath));
 	}
 	
 	@Before
@@ -37,12 +60,26 @@ public class DemoSiteLogin {
 		System.out.println( "Before" );
 		
 		// opens chrome
+		logExtentReport(Status.INFO, "Opening Chrome");
 		webDriver = new ChromeDriver();
 		webDriver.manage().window().maximize();
+		logExtentReport(Status.INFO, "Successfully opened Chrome");
+		
+		//java.util.List<String> mVals = mReader.readRow(0, "Sheet1");
+		
+		
+
+		mReader = new SpreadSheetReader(mLookupDoc);
+		userNametxt = mReader.readCell(0, 1, "Sheet1");
+		pwordtxt = mReader.readCell(1, 1, "Sheet1");
+		
+		logExtentReport(Status.INFO, "Username is : " + userNametxt);
+		logExtentReport(Status.INFO, "Password is : " + pwordtxt);
+		
 	}
 	
 	@Test
-	public void SignIn()
+	public void Test()
 	{
 		mHomePage = PageFactory.initElements(webDriver, Homepage.class);
 		
@@ -50,7 +87,15 @@ public class DemoSiteLogin {
 		System.out.println( "Test" );
 		
 		// navs to the specified URL
+		logExtentReport(Status.INFO, "Going to this URL: " + url);
 		webDriver.navigate().to(url);
+		
+		try {
+			logExtentReport(Status.INFO, "Screenshot taken" + ScreenShot.take(webDriver, "test"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			logExtentReport(Status.WARNING, "Unabe to take ScreenShot" + e.getMessage());
+		}
 		
 		// add the user
 		//---------------------
@@ -60,7 +105,7 @@ public class DemoSiteLogin {
 		// fills in the user name
 		mHomePage.setUserNameSignUp(userNametxt);
 		
-		// fills in the Password
+		// fills in the INFOword
 		mHomePage.setPasswordSignUp(pwordtxt);
 		
 		// click the save button
@@ -74,12 +119,19 @@ public class DemoSiteLogin {
 		// fills in the user name
 		mHomePage.setUserNameLogIn(userNametxt);
 		
-		// fills in the password
+		// fills in the INFOword
 		mHomePage.setPasswordLogIn(pwordtxt);
 		
 		// log the user in
 		mHomePage.clickLogInBtn();
 		
+		try {
+			logExtentReport(Status.INFO, "Screenshot taken of login status" + ScreenShot.take(webDriver, "logginIn"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			logExtentReport(Status.WARNING, "Unabe to take ScreenShot" + e.getMessage());
+		}
+				
 		// URL check 
 		String currentURL = webDriver.getCurrentUrl();
 		String expectedURL = "http://thedemosite.co.uk/login.php";
@@ -90,8 +142,13 @@ public class DemoSiteLogin {
 		WebElement logInCheck = webDriver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > big > blockquote > blockquote > font > center > b"));
 		
 		String check = logInCheck.getText();
-		assertEquals("**Successful Login**", check);
-		System.out.println( check );
+		try {
+			assertEquals("**Successful Login**", check);
+			logExtentReport(Status.PASS, "User Login status : " + check);
+		} catch (Exception e) {
+			logExtentReport(Status.ERROR, e.getMessage());
+		}
+		
 	}
 	
 	@After
@@ -109,5 +166,13 @@ public class DemoSiteLogin {
 	{
 		// runs last -- after every thing 
 		System.out.println( "After Class" );
+		
+		report.flush();
+	}
+	
+	private void logExtentReport(Status type, String msg)
+	{
+		//ExtentTest test = report.createTest("MyFirstTest");
+        test.log(type, msg);
 	}
 }
--- Testing/Automated_Testing/src/test/java/qa/consulting/com/Automated_Testing/Homepage.java
+++ Testing/Automated_Testing/src/test/java/qa/consulting/com/Automated_Testing/Homepage.java
@@ -28,7 +28,7 @@ public class Homepage {
 	
 	@FindBy(css="\r\n" + "body > table > tbody > tr > td.auto-style1 > form > div > center > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2) > p > input[type=\"button\"]")
 	WebElement mLogInBtn;
-	
+
 	
 	public void setUserNameSignUp(String userName)
 	{
